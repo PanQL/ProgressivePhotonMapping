32f5610cab891ad1932d4df3622e89a43fa631af
PanQL <panqinglin00@163.com>
2020-02-29 18:54:58 +0800

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,15 @@
 #![allow(dead_code)]
 #![feature(vec_resize_default)]
 
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 extern crate env_logger;
 extern crate lodepng;
-extern crate rgb;
 extern crate nalgebra;
 extern crate rand;
-pub mod util;
-pub mod scene;
+extern crate rgb;
 pub mod camera;
 mod consts;
 pub mod core;
+pub mod scene;
+pub mod util;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![allow(unused_imports)]
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 extern crate env_logger;
 
 use env_logger::Env;
-use ppm::util::*;
 use ppm::camera::Camera;
+use ppm::core::ProgressivePhotonTracer;
 use ppm::scene::Scene;
-use ppm::core::Render;
+use ppm::util::*;
+use std::sync::Arc;
 
 use ppm::scene::primitive::BazierCurve;
 
@@ -21,6 +23,6 @@ fn main() {
     camera.set_size(512, 384);
     camera.set_pos(&Vector3::new(6000.0, 5000.0, 400.0));
     camera.set_dir(Vector3::new(-1.0, 0.0, 0.0));
-    let mut render = Render::new(camera, scene);
-    render.run_ppm(1, 4);
+    let mut ppm = ProgressivePhotonTracer::new(Arc::new(camera), Arc::new(scene));
+    ppm.run(1, 4);
 }
